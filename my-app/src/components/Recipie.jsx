c458110f9e54cc258db28dbfb1d3d4c97541107c
Prajakta-Dhale <141712592+Prajakta-Dhale@users.noreply.github.com>
2025-12-02 11:28:19 +0000

    Recipe2

--- my-app/src/components/Recipie.jsx
+++ my-app/src/components/Recipie.jsx
@@ -6,11 +6,11 @@ export default function Recipe()
     const [recipename,setRecipeName]=useState("");
     const [ingredients,setIngredients]=useState("");
     const [instrucions,setInstrucions]=useState("");
-    const [recipe,setRecipes]=useState("");
+    const [recipes,setRecipes]=useState("");
 
     // Save Recipes to localstorage
     const saveRecipes=(updatedRecipes)=>{
-        localStorage.setItem("recipes",JSON.stringify(updaedRecipes));
+        localStorage.setItem("recipes",JSON.stringify(updatedRecipes));
         setRecipes(updatedRecipes);
     }
 
@@ -32,7 +32,7 @@ export default function Recipe()
                 ingredients,
                 instrucions
             }
-            saveRecipes([...setRecipes,newRecipe]);
+            saveRecipes([...recipes,newRecipe]);
         }
         setRecipeName("");
         setIngredients("");
@@ -42,20 +42,20 @@ export default function Recipe()
         <>
         <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px"}}>
             <h1 style={{marginBottom:"20px",border:"1px solid #ccc",padding:"20px"}}>Recipe Book</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div style={{marginBottom:"10px"}}>
                 <label>Name:</label><br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={recipename} onChange={(e)=>setRecipeName(e.target.value)} />
             </div>
 
             <div style={{marginBottom:"10px"}}>
                 <label>Ingredients:</label><br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={ingredients} onChange={(e)=>setIngredients(e.target.value)}/>
             </div>
 
             <div style={{marginBottom:"10px"}}>
                 <label>Instructions:</label><br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={instrucions} onChange={(e)=>setInstrucions(e.target.value)}/>
             </div>
 
             <button style={{padding:"5px 10px"}} type="submit">Add Recipe</button>
